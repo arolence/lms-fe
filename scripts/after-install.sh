1f3dc09317e75b1c8745cff9b12fcc02d38ebcd2
arolence <108116321+arolence@users.noreply.github.com>
2022-10-15 14:49:31 +0100

    Update after-install.sh

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -3,6 +3,4 @@ echo Build started on `date` &&
 npm install &&
 npm install -g @angular/cli &&
 ng build --configuration production  &&
-docker build user-app:latest .
-#mvn install
-#mvn jib:dockerBuild
+docker build -t user-web-app:latest .
