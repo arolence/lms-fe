8fda15400d6a922ae35ed908362629477c7c3d59
arolence <108116321+arolence@users.noreply.github.com>
2022-10-15 14:41:30 +0100

    Update boot.sh

--- scripts/boot.sh
+++ scripts/boot.sh
@@ -3,7 +3,7 @@ npm install &&
 npm install -g @angular/cli &&
 ng build --configuration production  &&
 
-docker build -t user-app:latest . &&
+docker build -t user-web-app:latest . &&
 
 docker-compose up -d
 
